bf235884617df0213aa0242b555d04e08c6399f6
Alexander Muth <almu.hp@arcor.de>
2019-10-20 17:29:28 +0200

    tiny refactoring

--- tests/src/alibraryloader_test.cpp
+++ tests/src/alibraryloader_test.cpp
@@ -13,7 +13,7 @@ typedef void(*releaseFunc)(void*);
 GTEST_TEST(ALibraryLoader_Test, load_getSymbol_unload_single)
 {
 	void* handle = apl::ALibraryLoader::load("libraries/first/first_lib");
-	ASSERT_TRUE(handle);
+	ASSERT_NE(handle, nullptr);
 
 	addFunc add = reinterpret_cast<addFunc>(apl::ALibraryLoader::getSymbol(handle, "add"));
 	subFunc sub = reinterpret_cast<subFunc>(apl::ALibraryLoader::getSymbol(handle, "sub"));
@@ -22,12 +22,12 @@ GTEST_TEST(ALibraryLoader_Test, load_getSymbol_unload_single)
 	subFunc subImg = reinterpret_cast<subFunc>(apl::ALibraryLoader::getSymbol(handle, "subImg"));
 	auto setToCharImg = apl::ALibraryLoader::getSymbol<setToCharFunc>(handle, "setToCharImg");
 
-	ASSERT_TRUE(add);
-	ASSERT_TRUE(sub);
-	ASSERT_TRUE(setToChar);
-	ASSERT_FALSE(addImg);
-	ASSERT_FALSE(subImg);
-	ASSERT_FALSE(setToCharImg);
+	ASSERT_NE(add, nullptr);
+	ASSERT_NE(sub, nullptr);
+	ASSERT_NE(setToChar, nullptr);
+	ASSERT_EQ(addImg, nullptr);
+	ASSERT_EQ(subImg, nullptr);
+	ASSERT_EQ(setToCharImg, nullptr);
 
 	ASSERT_EQ(add(12.5, 15), 27.5);
 	ASSERT_EQ(sub(15, 12.5), 2.5);
@@ -46,10 +46,10 @@ GTEST_TEST(ALibraryLoader_Test, load_getSymbol_unload_mutiple)
     void* imaginary2 = apl::ALibraryLoader::load("libraries/invalid/imaginary2/imaginary2_lib");
     void* second = apl::ALibraryLoader::load("libraries/second/second_lib");
 
-    ASSERT_FALSE(imaginary1);
-    ASSERT_TRUE(first);
-    ASSERT_FALSE(imaginary2);
-    ASSERT_TRUE(second);
+    ASSERT_EQ(imaginary1, nullptr);
+    ASSERT_NE(first, nullptr);
+    ASSERT_EQ(imaginary2, nullptr);
+    ASSERT_NE(second, nullptr);
 
     addFunc add = reinterpret_cast<addFunc>(apl::ALibraryLoader::getSymbol(first, "add"));
     subFunc sub = reinterpret_cast<subFunc>(apl::ALibraryLoader::getSymbol(first, "sub"));
@@ -58,12 +58,12 @@ GTEST_TEST(ALibraryLoader_Test, load_getSymbol_unload_mutiple)
     subFunc subImg = reinterpret_cast<subFunc>(apl::ALibraryLoader::getSymbol(first, "subImg"));
     auto setToCharImg = apl::ALibraryLoader::getSymbol<setToCharFunc>(first, "setToCharImg");
 
-    ASSERT_TRUE(add);
-    ASSERT_TRUE(sub);
-    ASSERT_TRUE(setToChar);
-    ASSERT_FALSE(addImg);
-    ASSERT_FALSE(subImg);
-    ASSERT_FALSE(setToCharImg);
+    ASSERT_NE(add, nullptr);
+    ASSERT_NE(sub, nullptr);
+    ASSERT_NE(setToChar, nullptr);
+    ASSERT_EQ(addImg, nullptr);
+    ASSERT_EQ(subImg, nullptr);
+    ASSERT_EQ(setToCharImg, nullptr);
 
     ASSERT_EQ(add(12.5, 15), 27.5);
     ASSERT_EQ(sub(15, 12.5), 2.5);
@@ -77,13 +77,13 @@ GTEST_TEST(ALibraryLoader_Test, load_getSymbol_unload_mutiple)
     allocateFunc allocateImg = reinterpret_cast<allocateFunc>(apl::ALibraryLoader::getSymbol(second, "allocateImg"));
     auto releaseImg = apl::ALibraryLoader::getSymbol<releaseFunc>(second, "releaseImg");
 
-    ASSERT_TRUE(allocate);
-    ASSERT_TRUE(release);
-    ASSERT_FALSE(allocateImg);
-    ASSERT_FALSE(releaseImg);
+    ASSERT_NE(allocate, nullptr);
+    ASSERT_NE(release, nullptr);
+    ASSERT_EQ(allocateImg, nullptr);
+    ASSERT_EQ(releaseImg, nullptr);
 
     void* ptr = allocate(sizeof(char));
-    ASSERT_TRUE(ptr);
+    ASSERT_NE(ptr, nullptr);
     release(ptr);
 
     void* img1 = apl::ALibraryLoader::getSymbol(imaginary1, "add");
@@ -91,10 +91,10 @@ GTEST_TEST(ALibraryLoader_Test, load_getSymbol_unload_mutiple)
     void* img3 = apl::ALibraryLoader::getSymbol(imaginary2, "allocate");
     void* img4 = apl::ALibraryLoader::getSymbol(imaginary2, "release");
 
-    ASSERT_FALSE(img1);
-    ASSERT_FALSE(img2);
-    ASSERT_FALSE(img3);
-    ASSERT_FALSE(img4);
+    ASSERT_EQ(img1, nullptr);
+    ASSERT_EQ(img2, nullptr);
+    ASSERT_EQ(img3, nullptr);
+    ASSERT_EQ(img4, nullptr);
 
     ASSERT_TRUE(apl::ALibraryLoader::unload(imaginary1));
     ASSERT_TRUE(apl::ALibraryLoader::unload(first));
