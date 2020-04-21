d344ad27e5059a3187623fb14b0ad6cf3a07bec1
Alexander Muth <almu.hp@arcor.de>
2020-04-21 16:14:31 +0200

    Fixed wrong and missing documentation

--- doc/plugininfos.cpp
+++ doc/plugininfos.cpp
@@ -1,59 +1,90 @@
 /**
- * @class apl::PluginFeatureInfo
+ * @struct apl::PluginFeatureInfo
  *
  * @brief For every feature in a plugin, a new PluginFeatureInfo is generated with infos about this feature.
- */
-
-/**
+ *
+ * @var apl::PluginFeatureInfo::pluginInfo
+ * A pointer to the apl::PluginInfo object of this plugin.
+ *
  * @var apl::PluginFeatureInfo::featureGroup
- *
- * The name of the feature group of the feature.
- */
-/**
+ * The group of this feature.
  * @var apl::PluginFeatureInfo::featureName
+ * The name of this feature.
  *
- * The name of the feature.
- */
-/**
  * @var apl::PluginFeatureInfo::returnType
+ * The return type of this feature.
  *
- * The return type of the feature.
- */
-/**
  * @var apl::PluginFeatureInfo::parameterList
+ * The parameter list of this feature.
+ * @var apl::PluginFeatureInfo::parameterTypes
+ * The types of the parameters in the parameter list of this feature.
+ * @var apl::PluginFeatureInfo::parameterNames
+ * The names of the parameters in the parameter list of this feature.
  *
- * The parameter list of the feature.
- */
-/**
  * @var apl::PluginFeatureInfo::functionPointer
- *
  * The pointer to the feature function.
  */
 
 
 /**
- * @class apl::PluginClassInfo
+ * @struct apl::PluginClassInfo
  *
  * @brief For every class in a plugin, a new PluginClassInfo is generated with infos about this class.
+ *
+ * @var apl::PluginClassInfo::pluginInfo
+ * A pointer to the apl::PluginInfo object of this plugin.
+ *
+ * @var apl::PluginClassInfo::interfaceName
+ * The name of the interface from which this class inherits.
+ *
+ * @var apl::PluginClassInfo::className
+ * The name of the class.
+ *
+ * @var apl::PluginClassInfo::createInstance
+ * The (factory) method to create an instance of the class.
+ * @var apl::PluginClassInfo::deleteInstance
+ * The method to delete an instance of the class.
  */
 
+
 /**
- * @var apl::PluginClassInfo::interfaceName
+ * @struct apl::PluginInfo
  *
- * The name of the interface from which this class inherits.
- */
-/**
- * @var apl::PluginClassInfo::className
+ * @brief For every plugin, a new PluginInfo is generated with infos about this plugin.
  *
- * The name of the class.
- */
-/**
- * @var apl::PluginClassInfo::createInstance
+ * @var apl::PluginInfo::pluginName
+ * The name of this plugin (empty if not set)
  *
- * The (factory) method to create an instance of the class.
- */
-/**
- * @var apl::PluginClassInfo::deleteInstance
+ * @var apl::PluginInfo::apiVersionMajor
+ * The major of the API version of APluginSDK.
+ * @var apl::PluginInfo::apiVersionMinor
+ * The minor of the API version of APluginSDK.
+ * @var apl::PluginInfo::apiVersionPatch
+ * The patch of the API version of APluginSDK.
  *
- * The method to delete an instance of the class.
+ * @var apl::PluginInfo::pluginVersionMajor
+ * The major of the API version of this plugin.
+ * @var apl::PluginInfo::pluginVersionMinor
+ * The minor of the version of this plugin.
+ * @var apl::PluginInfo::pluginVersionPatch
+ * The patch of the version of this plugin.
+ *
+ * @var apl::PluginInfo::allocateMemory
+ * The allocate function of this plugin.
+ * @var apl::PluginInfo::freeMemory
+ * The free function of this plugin.
+ *
+ * @var apl::PluginInfo::getFeatureCount
+ * Function pointer that returns the number of registered features in this plugin.
+ * @var apl::PluginInfo::getFeatureInfo
+ * Function pointer that returns the feature at the passed index in this plugin.
+ * @var apl::PluginInfo::getFeatureInfos
+ * Function pointer that returns an array of all registered features in this plugin.
+ *
+ * @var apl::PluginInfo::getClassCount
+ * Function pointer that returns the number of registered classes in this plugin.
+ * @var apl::PluginInfo::getClassInfo
+ * Function pointer that returns the class at the passed index in this plugin.
+ * @var apl::PluginInfo::getClassInfos
+ * Function pointer that returns an array of all registered classes in this plugin.
  */
--- src/pluginmanagerobserver.cpp
+++ src/pluginmanagerobserver.cpp
@@ -1,3 +1,12 @@
 #include "APluginLibrary/pluginmanagerobserver.h"
 
+/**
+ * @class apl::PluginManagerObserver
+ *
+ * @brief Observes the loading and unloading of plugins in PluginManager.
+ *
+ * With the PluginManagerObserver you can receive notifications if a plugin in the observerd PluginManager object(s) is
+ * loaded or unloaded.
+ */
+
 apl::PluginManagerObserver::~PluginManagerObserver() = default;
